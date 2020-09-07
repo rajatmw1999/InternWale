117f996dbcbc8a07c6a83b8314f7e27b706d5e55
shivam7374 <shivamsingla491@gmail.com>
2020-09-07 21:23:21 +0530

    Add

---
+++ backend/models/Job.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+
+const Roadmap_Data = new mongoose.Schema({
+  NameOfRoadmap: {
+    type: String,
+  },
+  NameofAuthor: {
+    type: String,
+  },
+  DatePublished: {
+    type: Date,
+  },
+  Likes: {
+    type: Number,
+  },
+  ImageLink: {
+    type: String,
+  },
+  Category: {
+    type: String,
+  },
+  Tags: [
+    {
+      type: String,
+    },
+  ],
+  Text: {
+    type: String,
+  },
+  Timeline: [
+    {
+      name: {
+        type: String,
+      },
+      desc: {
+        type: String,
+      },
+      time: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+module.exports = RoadmapData = mongoose.model("RoadmapData", Roadmap_Data);
