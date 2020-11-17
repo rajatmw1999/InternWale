8af527c016b3a90a14a86c0d60490326a03b0492
sarthak bhatia <56428721+sarthakbhatia106@users.noreply.github.com>
2020-11-17 23:39:56 +0530

    Update intelRouter.js

--- backend/routes/intelRouter.js
+++ backend/routes/intelRouter.js
@@ -1,6 +1,6 @@
 const express= require("express")
 const Router= express.Router();
 
-const scraper=require("./scraper")
+const scraper=require("../scrappers/intel_scraper/scraper.js")
 
 scraper();
