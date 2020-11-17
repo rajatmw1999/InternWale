bf007d581183009e1e869363876f3d01f384e84a
sarthak bhatia <56428721+sarthakbhatia106@users.noreply.github.com>
2020-11-17 23:38:52 +0530

    Update microsoftRouter.js

--- backend/routes/microsoftRouter.js
+++ backend/routes/microsoftRouter.js
@@ -1,6 +1,6 @@
 const express= require("express")
 const Router= express.Router();
 
-const scraper=require("./finalScraper")
+const scraper=require("../scrappers/microsoft_scraper/finalScraper")
 
 scraper();
