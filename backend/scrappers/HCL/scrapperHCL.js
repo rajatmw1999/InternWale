065d4511a5c59a155963c090485c236d06a35ca2
Himank Goel <51325629+himankgoel@users.noreply.github.com>
2020-11-17 20:29:45 +0530

    Added HCL scrapper

---
+++ backend/scrappers/HCL/scrapperHCL.js
@@ -0,0 +1,34 @@
+const puppeteer = require("puppeteer");
+async function scrapperHCL() {
+  let fullData = [];
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(`https://www.hcltech.com/careers/Careers-in-india`, {
+    timeout: 0,
+  });
+
+  const data = await page.evaluate(
+    () => {
+      const temp = [];
+      const tbodies = document.getElementsByTagName("tbody")[0].children;
+      for (const tbody of tbodies) {
+        const nameOfJob = tbody.children[0].children[0].text;
+        const link = tbody.children[0].children[0].href;
+        const location = tbody.children[2].innerText;
+        const dateAdded = tbody.children[1].innerText;
+        const expReq = tbody.children[3].innerText;
+        temp.push({ nameOfJob, link, location, dateAdded, expReq });
+      }
+      return temp;
+    },
+    { timeout: 0 }
+  );
+
+  for (let j = 0; j < data.length; j++) {
+    fullData.push(data[j]);
+  }
+  await browser.close();
+  return fullData;
+}
+// module.exports = scrapperHCL;
+scrapperHCL().then((res) => console.log(res));
