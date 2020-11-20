d07691799f3d7fc31ab9efcdd50828574c480d74
sarthak bhatia <56428721+sarthakbhatia106@users.noreply.github.com>
2020-11-20 11:41:49 +0530

    added tokio life

---
+++ backend/scrappers/tokio life/scraper.js
@@ -0,0 +1,48 @@
+const puppeteer = require('puppeteer');
+
+async function scraper (){
+    const extractJobs= async(url)=>{
+        const page = await browser.newPage();
+        
+        await page.goto(url,{waitUntil:"networkidle2"});
+        
+        let jobData = await page.evaluate(() => {
+          let jobs = [];
+          
+          let jobElms = document.querySelectorAll('article.jobTuple.bgWhite.br4.mb-8');
+          
+          jobElms.forEach((job) => {
+              let jobJson = {};
+              try {
+                jobJson.name = job.querySelector('a.title.fw500.ellipsis').innerText;
+                jobJson.location = job.querySelector('li.location span').innerText;
+                jobJson.description=job.querySelector('div.job-description').innerText;
+                
+                var link=job.querySelector('a.title.fw500.ellipsis')
+                jobJson.link= link.getAttribute('href')
+                }
+              catch (exception){
+      
+              }
+              jobs.push(jobJson);
+            });
+          return jobs;
+        });
+    
+        
+        return jobData;
+      }
+    
+    
+      const browser = await puppeteer.launch();
+      
+      const url="https://www.naukri.com/edelweiss-tokio-life-insurance-jobs"
+    
+      const jobData= await extractJobs(url)
+      console.log(jobData)
+        
+
+      await browser.close();
+}
+
+module.exports= scraper;
