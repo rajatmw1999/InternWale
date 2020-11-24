b038bbc9cd03ff6f59c1e1c73e4cf92fe1efeef2
sarthak bhatia <56428721+sarthakbhatia106@users.noreply.github.com>
2020-11-24 19:46:20 +0530

    Add files via upload

---
+++ backend/routes/s20/index.js
@@ -0,0 +1,45 @@
+const scraper= require("../scrappers/axa Linguistics/scraper.js");
+const app= require("express")();
+const mongoose= require('mongoose');
+const job=require("../models/jobs");
+
+app.get("/s20",function (req,res){
+    scraper().then(fullData=>{
+        let dataArray = [];
+        let itr=0;
+        for(data in fullData)
+        {
+            itr++;
+            let newJob = {
+            Title:data.name,
+            Category:null,
+            DatePosted: null,
+            Company: data.company,
+            LinktoJobPost: data.link,
+            Description: data.description,
+            JobId:null,
+            Location:data.location
+            };
+            dataArray.push(newJob);
+            if(itr==20){
+            break;
+            }
+        }
+
+        const newData = new job({
+        CompanyName:"AXA",
+        DateScrap:Date.now(),
+        UID:"axa_20",
+        Data:dataArray
+        });
+
+        newData.save();
+        console.log("data saved in database")
+        })
+});
+
+// app.listen(3000,(req,res)=>{
+//     console.log("app listening at http://localhost:3000")
+// })
+
+module.exports=app;
