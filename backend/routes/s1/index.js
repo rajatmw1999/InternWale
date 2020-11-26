093411b9ed8d1a19e59fe3ca58935b174079ade5
sarthak bhatia <56428721+sarthakbhatia106@users.noreply.github.com>
2020-11-26 19:15:59 +0530

    Update index.js

--- backend/routes/s1/index.js
+++ backend/routes/s1/index.js
@@ -1,43 +1,35 @@
-const scraper= require("../scrappers/Accenture/scrapperAccenture.js");
-const app= require("express")();
-const mongoose= require('mongoose');
-const job=require("../models/jobs");
+const router = require("express").Router();
+const scrapper = require("../../scrappers/Accenture/scrapperAccenture");
+const Job = require("../../models/Job");
+const data = [];
 
-app.get("/s1",(req,res)=>{
-    scraper().then(fullData=>{
-        let dataArray = [];
-        let itr=0;
-        for(data in fullData)
-        {
-            itr++;
-            let newJob = {
-            Title:data.nameOfJob,
-            Category:null,
-            DatePosted: null,
-            Company: data.company,
-            LinktoJobPost: data.link,
-            Description: null,
-            JobId:null,
-            Location:data.location
-            };
-            dataArray.push(newJob);
-            if(itr==20){
-            break;
-            }
-        }
-
-        const newData = new job({
-        CompanyName:"Accenture",
-        DateScrap:Date.now(),
-        UID:"accenture_1",
-        Data:dataArray
-        });
-
-        newData.save();
-    })
+router.get("/s1", async (req, res)=> {
+  let data = [];
+  await scrapper().then((jobs) => {
+    for(let i = 0; i < jobs.length && i < 20; i++) {
+      const new_job = {
+        Title: jobs[i].nameOfJob || null,
+        Category: jobs[i].category || null,
+        DatePosted: jobs[i].date || null,
+        Company: jobs[i].company,
+        LinktoJobPost: jobs[i].link || null,
+        JobId: null,
+        Description: jobs[i].description || null,
+        Location: jobs[i].location || null,
+      };
+      data.push(new_job);
+    }
+  })
+  .then(async (ans)=>{
+    const newData = await new Job({
+      CompanyName: "Accenture",
+      DateScrap: Date.now(),
+      UID: "accenture_1",
+      Data: data,
+    });
+    console.log(newData)
+    
+  });
 });
 
-// app.listen(3000,(req,res)=>{
-//     console.log("app listening at http://localhost:3000")
-// })
-module.exports=app;
+module.exports = router;
