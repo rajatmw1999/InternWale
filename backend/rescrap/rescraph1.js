c7fad6f63adb5c719f9cfd9372a70b6975c6f74b
unknown <rajatis1999@gmail.com>
2020-11-26 21:31:22 +0530

    rescraph1 added

---
+++ backend/rescrap/rescraph1.js
@@ -0,0 +1,4 @@
+//1. write you route here
+//2. import the scrapped data from the scrapper file in the scapper folder that you have created for the website http://thiscourse.me/Checking-Rescrap/
+//3. inform me about this and I will add some more data.
+//4. run route and check if it is detecting the newly scrapped data and logging it
