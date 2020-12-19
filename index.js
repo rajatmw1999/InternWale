000239ca40852e01243f62bcdd26a26ff0fec951
himankgoel <himankgoel12@gmail.com>
2020-12-19 22:46:57 +0530

    Added h23-rescrap for testing(OK)

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-// const URI = `mongodb+srv://user:s3oTPRp8I89vZ6qC@cluster0.r2ehn.mongodb.net/Jobs?retryWrites=true&w=majority`;
-// mongoose
-//   .connect(URI, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB Connected..."))
-//   .catch((err) => console.log(err));
-mongoose.connect(
-  "mongodb+srv://admin:admin@cluster0-nbxxl.mongodb.net/jobsSkillUnga?retryWrites=true&w=majority",
-  {
-    //useMongoClient: true
+const URI = `mongodb+srv://user:s3oTPRp8I89vZ6qC@cluster0.r2ehn.mongodb.net/Jobs?retryWrites=true&w=majority`;
+mongoose
+  .connect(URI, {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
-  },
-  console.log("Database Connected")
-);
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+// mongoose.connect(
+//   "mongodb+srv://admin:admin@cluster0-nbxxl.mongodb.net/jobsSkillUnga?retryWrites=true&w=majority",
+//   {
+//     //useMongoClient: true
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   console.log("Database Connected")
+// );
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
