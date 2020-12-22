65127b98f6cfe36046d87da603ea20f5dd24a147
unknown <rajatis1999@gmail.com>
2020-12-22 14:10:13 +0530

    models for job scrapping from platforms

---
+++ backend/PlatformJobs/models/ShineIntern.js
@@ -0,0 +1,80 @@
+const { Number } = require("mongoose");
+const mongoose = require("mongoose");
+
+const ShineInternSchema = new mongoose.Schema({
+   UID: {
+    type: String,
+    required:true
+  },
+  PlatformName: {
+    type: String,
+    required:true
+  },
+  DateScrap: {
+    type: String,
+    default: Date.now(),
+  },
+  TotalField:{
+    type:Number,
+    required:true
+  },
+  TotalSubfield:{
+    type:Number,
+    required:true
+  },
+  TotalJobs:{
+    type:Number,
+    required:true
+  },
+  Data: 
+    {
+        NameOfField:{
+            type:String,
+            required:true
+        },
+        NameOfSubfield:{
+            type:String,
+            required:true
+        },
+        JobsArr:[{
+            JobId: {
+                type: String,
+                default:null
+            },
+            Title: {
+                type: String,
+                required:true
+            },
+            DatePosted: {
+                type: String,
+                default:null
+            },
+            CompanyName: {
+                type: String,
+                required:true
+            },
+            LinktoJobPost: {
+                type: String,
+                required:true
+            },        
+            Description: {
+                type: String,
+                required:true
+            },
+            Location: {
+                type: String,
+                required:true
+            },
+            Stipend:{
+                type:String,
+                default:null
+            },
+            Skills:{
+                type:String,
+                default:null
+            }
+        }]
+    },
+});
+
+module.exports = ShineIntern = mongoose.model("ShineIntern", ShineInternSchema);
